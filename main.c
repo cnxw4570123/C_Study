5e1d8d200adb08f7c5ed6165d3bd4328e8d1bce1
JGM <cnxw4570123@gmail.com>
2023-03-12 16:42:35 +0900

    calc input words max length

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-void print_binary(int *ch, int num);
+int max_length(char *str, int size);
 
 int main(void ){
-    int bin[32] = {0};
-    int character;
-    scanf("%c", &character);
-    print_binary(bin, character);
-    printf("%d\n", (char)character);
+    char str[100];
+    int max = 0, value;
+    while (1){
+        value = max_length(str, 100);
+        if(max < value) max = value;
+        if(value == EOF) break;
+    }
+    printf("가장 긴 단어의 길이 : %d", max);
     return 0;
 }
 
-void print_binary(int *ch, int num) {
-    int *last_bit = ch + 31;
-    while(num > 2){
-        *(last_bit--) = num % 2;
-        num /= 2;
+int max_length(char *str, int size){
+    int ch, max = 0;
+    ch = getchar();
+    if(ch == EOF) return EOF;
+    while(ch != '\n'){
+        *str++ = ch;
+        ch = getchar();
+        max++;
     }
-    *last_bit = num % 2;
-    *(last_bit - 1) = num / 2;
-    for(int i = 0; i < 32; i++){
-        printf("%u", *(ch + i));
-        if((i+1) % 8 == 0) printf(" ");
-    }
-    printf("\n");
+    return max;
 }
-
