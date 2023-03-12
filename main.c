6b662836263050373c4b51ba0df113053c86a942
JGM <cnxw4570123@gmail.com>
2023-03-12 14:15:18 +0900

    print_binary

--- main.c
+++ main.c
@@ -1,42 +1,28 @@
 #include <stdio.h>
 
-void input_no(int *lotto);
-void print_no(int *lotto);
+void print_binary(int *ch, int num);
+
 int main(void ){
-    int lotto_number[6];
-    input_no(lotto_number);
-    print_no(lotto_number);
+    int bin[32] = {0};
+    int character;
+    scanf("%c", &character);
+    print_binary(bin, character);
+    printf("%d\n", (char)character);
     return 0;
 }
-// 3 7 15 22 35 40
-void input_no(int *lotto){
-    int *start = lotto;
-    while (lotto < start + 6){
-        int number;
-        printf("enter number between 1 and 45 : ");
-        scanf("%d", &number);
-        if(number < 1 || number > 45){
-            printf("%d is not in range.\n", number);
-            continue;
-        }
 
-        int is_duplicate = 0;
-        for(int i = 0; i < lotto - start; i++){
-            if(number == *(start + i)){
-                printf("%d is in lotto\n", number);
-                is_duplicate = 1;
-                continue;
-            }
-        }
-        if(!is_duplicate){
-            *lotto = number;
-            lotto++;
-        }
+void print_binary(int *ch, int num) {
+    int *last_bit = ch + 31;
+    while(num > 2){
+        *(last_bit--) = num % 2;
+        num /= 2;
     }
-}
-void print_no(int *lotto) {
-    printf("lotto numbers : ");
-    for(int i = 0; i < 6; i++){
-        printf("%-5d", *lotto++);
+    *last_bit = num % 2;
+    *(last_bit - 1) = num / 2;
+    for(int i = 0; i < 32; i++){
+        printf("%u", *(ch + i));
+        if((i+1) % 8 == 0) printf(" ");
     }
+    printf("\n");
 }
+
