7ce7fabc1d748ae5f1f57eb984407f4255d5ab49
JGM <cnxw4570123@gmail.com>
2023-03-16 18:00:23 +0900

    pointer and array

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(void ){
-    char str[80];
-    char str2[80];
-    printf("input words : ");
-    scanf("%s", str);
-    strcpy(str2, str);
-    if(strlen(str2) > 5){
-        for (int i = 5; i < strlen(str2); i++) *(str2 + i) = '*';
+int main(void) {
+    int ary1[4] = {1, 2, 3, 4};
+    int ary2[4] = {11, 12, 13, 14};
+    int ary3[5] = {21, 22, 23, 24, 25};
+    int *pary[3] = {ary1, ary2, ary3};
+    int row = sizeof pary / sizeof pary[0];
+    int col = sizeof ary1 / sizeof ary1[0];
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            printf("%5d", *(*(pary+i)+ j));
+        }
+        printf("\n");
     }
-    printf("input words : %s, replaced words: %s\n", str, str2);
     return 0;
 }
