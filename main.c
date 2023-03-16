98b52e92303e3a8693c300a22c12fc9ca448a932
JGM <cnxw4570123@gmail.com>
2023-03-16 18:34:46 +0900

    add numbers

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-    int ary1[4] = {1, 2, 3, 4};
-    int ary2[4] = {11, 12, 13, 14};
-    int ary3[5] = {21, 22, 23, 24, 25};
-    int *pary[3] = {ary1, ary2, ary3};
-    int row = sizeof pary / sizeof pary[0];
-    int col = sizeof ary1 / sizeof ary1[0];
-    for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < col; ++j) {
-            printf("%5d", *(*(pary+i)+ j));
+    int rect[5][6] = {0};
+    int total, *pr;
+
+    for (int i = 0; i < 4; i++) {
+        total = 0, pr = rect[i];
+        for (int j = 0; j < 5; j++) {
+            rect[i][j] = i*5 + j + 1;
+            total += rect[i][j];
+            pr++;
+        }
+        *pr = total;
+    }
+
+    for (int i = 0; i < 6; ++i) {
+        total = 0;
+        pr = *(rect + 4);
+        for (int j = 0; j < 4; ++j) {
+            total += rect[j][i];
+        }
+        *(pr + i) = total;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            printf("%5d", rect[i][j]);
         }
         printf("\n");
     }
