e423815f992857d1dd42b69ce17bca1d5af28749
JGM <cnxw4570123@gmail.com>
2023-03-17 22:37:41 +0900

    swap numbers regardless of type

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+void swap(char *type, void *vp1, void *vp2);
 
 int main(void) {
-    int rect[5][6] = {0};
-    int total, *pr;
+    int age1, age2;
+    double height1, height2;
 
-    for (int i = 0; i < 4; i++) {
-        total = 0, pr = rect[i];
-        for (int j = 0; j < 5; j++) {
-            rect[i][j] = i*5 + j + 1;
-            total += rect[i][j];
-            pr++;
-        }
-        *pr = total;
-    }
+    printf("첫 번째 사람의 나이와 키 입력 : ");
+    scanf("%d%lf", &age1, &height1);
+    printf("%두 번째 사람의 나이와 키 입력 : ");
+    scanf("%d%lf", &age2, &height2);
 
-    for (int i = 0; i < 6; ++i) {
-        total = 0;
-        pr = *(rect + 4);
-        for (int j = 0; j < 4; ++j) {
-            total += rect[j][i];
-        }
-        *(pr + i) = total;
-    }
+    swap("int", &age1 ,&age2);
+    swap("double", &height1 ,&height2);
+    printf("첫 번째 사람의 나이와 키 : %d, %.1lf\n", age1, height1);
+    printf("두 번째 사람의 나이와 키 : %d, %.1lf\n", age2, height2);
 
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 6; ++j) {
-            printf("%5d", rect[i][j]);
-        }
-        printf("\n");
-    }
     return 0;
+}
+
+void swap(char *type, void *vp1, void *vp2){
+    if(strcmp(type, "int") == 0){
+        int temp = *(int *)vp1;
+//        int *temp = *(int *)vp1; 주소가 바뀌기 때문에 안에 있는 값 또한 달라짐
+        *(int *)vp1 = *(int *)vp2;
+        *(int *)vp2 = temp;
+//        *(int *)vp2 = *temp; 이렇게 사용할 시 이전에 가리키던 vp1의 주소가 vp2로 바뀌었기 때문에 vp2에 vp2를 할당하는 꼴이된다.
+    } else if(strcmp(type, "double") == 0){
+        double temp = *(double *)vp1;
+        *(double *)vp1 = *(double *)vp2;
+        *(double *)vp2 = temp;
+    }
 }
